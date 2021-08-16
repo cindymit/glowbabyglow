78799460c81cc7655755c9108f267eaf472f2658
Cindy Mit <cindymit89@gmail.com>
2021-08-16 12:07:35 -0400

    Updated services and app js, added more structure to files

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,63 @@
-
-import './App.css';
+import "./App.css";
+import Login from "./screens/Login/Login";
+import Signup from "./screens/Signup/Signup";
+import Layout from "./components/Layout/Layout";
+import Main from "./containers/Main";
+import { Switch, Route, useHistory } from "react-router-dom";
+import { useState, useEffect } from "react";
+import {
+  loginUser,
+  signupUser,
+  verifyUser,
+  removeToken,
+} from "./services/users";
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    const handleVerify = async () => {
+      const userData = await verifyUser();
+      setCurrentUser(userData);
+    };
+    handleVerify();
+  }, []);
+
+  const handleLogin = async (formData) => {
+    const userData = await loginUser(formData);
+    setCurrentUser(userData);
+    history.push("/");
+  };
+
+  const handleSignup = async (formData) => {
+    const userData = await signupUser(formData);
+    setCurrentUser(userData);
+    history.push("/");
+  };
+
+  const handleLogout = () => {
+    setCurrentUser(null);
+    localStorage.removeItem("authToken");
+    removeToken();
+    history.push("/");
+  };
+
   return (
     <div className="App">
-      <h1>Glow Baby Glow!</h1>
+      <Layout currentUser={currentUser} handleLogout={handleLogout}>
+        <Switch>
+          <Route path="/login">
+            <Login handleLogin={handleLogin} />
+          </Route>
+          <Route path="/signup">
+            <Signup handleSignup={handleSignup} />
+          </Route>
+          <Route path="/">
+            <Main />
+          </Route>
+        </Switch>
+      </Layout>
     </div>
   );
 }
